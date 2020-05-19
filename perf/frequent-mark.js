496d996548526802e8f2b19a419ce19678f82bca
AmyFoxFN <funanamy@gmail.com>
2020-05-19 16:54:08 +0800

    run well test case

--- perf/frequent-mark.js
+++ perf/frequent-mark.js
@@ -2,7 +2,13 @@
  * 
  */
 export default class FrequentMark {
+  _averageDuration = 0
+  _observer = null
+
   constructor({ stopThreshold = 200 } = {}) {
+    // setTimeout(() => {
+    //   this._observer && this._observer(1)
+    // }, 2000)
   }
 
   start() {
@@ -11,5 +17,9 @@ export default class FrequentMark {
 
   end() {
 
+  }
+
+  observe(observer) {
+    this._observer = observer
   }
 }
--- perf/frequent-mark.test.js
+++ perf/frequent-mark.test.js
@@ -1,7 +1,21 @@
 import FrequentMark from './frequent-mark'
 
-test('start', () => {
+
+test('run well', (done) => {
   const frequentMark = new FrequentMark()
 
-  expect(frequentMark).not.toBeNull()
+  const interval = setInterval(() => {
+    frequentMark.start()
+    const a = 1
+    frequentMark.end()
+  }, 20);
+
+  setTimeout(() => {
+    clearInterval(interval)
+  }, 1000)
+
+  frequentMark.observe((averageDuration) => {
+    expect(averageDuration).toBeGreaterThan(0)
+    done()
+  })
 })
