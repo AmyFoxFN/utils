9bf2ed506b6ad76fe01f30f56f7eb15e48ffefb5
AmyFoxFN <funanamy@gmail.com>
2020-05-20 08:55:30 +0800

    add babel plugins

--- babel.config.js
+++ babel.config.js
@@ -9,4 +9,10 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-transform-runtime"
+  ]
 };
